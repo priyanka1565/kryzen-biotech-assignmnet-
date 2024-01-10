6d92a1dad527fdccc88284b963ead5992537aecf
priyanka1565 <104352972+priyanka1565@users.noreply.github.com>
2024-01-10 19:03:19 +0530

    added

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
         }
 
         try {
-            const response = await fetch('https://kryzen-backend-jstz.onrender.com/users/login', {
+            const response = await fetch(' http://localhost:8080/users/login', {
                 method: 'POST',
                 body: JSON.stringify(formData),
                 headers: {
